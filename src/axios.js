9fb37e26345aec91598bc117db74e9758748a6be
GonzaloPulido <81957404+GonzaloPulido@users.noreply.github.com>
2025-02-06 12:54:53 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:8000',
-  baseURL: 'https://fastapicorsjwt.onrender.com/',
+  //baseURL: 'https://fastapicorsjwt.onrender.com/',
+  baseURL: 'https://fastapi-requestanalyzer.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
